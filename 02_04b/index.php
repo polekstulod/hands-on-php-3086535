98099a46d3ff386e6e143a639541584673fdde07
James Paul Tulod <paultulod@pm.me>
2022-12-20 22:42:20 +0800

    Finish 'Keep track of unique visitors'

--- 02_04b/index.php
+++ 02_04b/index.php
@@ -1,16 +1,33 @@
-<?php 
-	function get_visitors() {
-		return file_get_contents('visits.txt');
-	}
+<?php
+function get_visitors()
+{
+    return file_get_contents('visits.txt');
+}
+
+function log_visit()
+{
+    $visits = (int)file_get_contents('visits.txt');
+    $visits++;
+    file_put_contents('visits.txt', $visits);
+}
+
+if (!isset($_COOKIE['visited'])) {
+    setcookie('visited', 'yes', time() + 60 * 60 * 24 * 365);
+    log_visit();
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome to My Website</title>
-        <meta name="author" value="Joe Casabona" />
-    </head>
-    <body>
-        <h1>Welcome to My Website</h1>
-				<p>There have been <b><?php echo get_visitors(); ?></b> visitors so far.</p>
-    </body>
+
+<head>
+    <title>Welcome to My Website</title>
+    <meta name="author" value="Joe Casabona" />
+</head>
+
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>There have been <b><?php echo get_visitors(); ?></b> visitors so far.</p>
+</body>
+
 </html>
